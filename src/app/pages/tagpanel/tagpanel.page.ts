6afdc8be43af6854dd27738f5436e6a92041d177
harleytobias <harleytobias1@gmail.com>
2018-11-24 15:15:14 +0800

    working gps

--- src/app/pages/tagpanel/tagpanel.page.ts
+++ src/app/pages/tagpanel/tagpanel.page.ts
@@ -3,6 +3,9 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { SMS } from '@ionic-native/sms/ngx';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { SqliteService } from '../../services/util/sqlite.service';
+import { Toast } from '@ionic-native/toast/ngx';
+
+declare var AdvancedGeolocation:any;
 
 @Component({
   selector: 'app-tagpanel',
@@ -12,12 +15,15 @@ import { SqliteService } from '../../services/util/sqlite.service';
 export class tagpanelPage implements OnInit {
 
   formPanel: FormGroup;
-
+  isBarcodeScanned = false;
+  location;
+  
   constructor(
     private barcodeScanner: BarcodeScanner,
     private sms: SMS,
     private fb: FormBuilder,
-    private sql: SqliteService) {
+    private sql: SqliteService,
+    private toast: Toast) {
       this.formPanel = fb.group({
         panel_code: ['', [Validators.required]],
         panel_name: ['', [Validators.required]],
@@ -36,16 +42,24 @@ export class tagpanelPage implements OnInit {
     {value:"invited"},
     {value:"dropped"}]
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      this.start()
+      
+    }
+  
 
   scan(){
+    this.isBarcodeScanned = false
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+      this.isBarcodeScanned = true
       this.formPanel.get('panel_code').setValue(barcodeData.text)
+      console.log("location",`${this.location.latitude}, ${this.location.longitude}`)
+      this.formPanel.get('panel_gps_location').setValue(`${this.location.latitude}, ${this.location.longitude}`)
      }).catch(err => {
          console.log('Error', err);
      });
+
   }
 
   sendSMS(){
@@ -62,5 +76,79 @@ export class tagpanelPage implements OnInit {
                    `${formValue.panel_receipted}`
     return message 
   }
+  
+  start(){
+  
+    AdvancedGeolocation.start(function(success){
 
+        try{
+          var jsonObject = JSON.parse(success);
+          this.location = jsonObject
+          
+          console.log("Provider now " +JSON.stringify(jsonObject));
+          this.showToast(JSON.stringify(jsonObject))
+          switch(jsonObject.provider){
+            case "gps":
+              //TODO
+              break;
+
+            case "network":
+              //TODO
+              break;
+
+            case "satellite":
+              //TODO
+              break;
+
+            case "cell_info":
+              //TODO
+              break;
+
+            case "cell_location":
+              //TODO
+              break;
+
+            case "signal_strength":
+              //TODO
+              break;
+          }
+        }
+        catch(exc){
+          //this.showToast("value"+exc)
+          console.log("Invalid JSON: " + exc);
+        }
+      },
+      function(error){
+        this.showToast(JSON.stringify(error))
+        console.log("ERROR! " + JSON.stringify(error));
+      },
+      ////////////////////////////////////////////
+      //
+      // REQUIRED:
+      // These are required Configuration options!
+      // See API Reference for additional details.
+      //
+      ////////////////////////////////////////////
+      {
+        "minTime":500,         // Min time interval between updates (ms)
+        "minDistance":1,       // Min distance between updates (meters)
+        "noWarn":true,         // Native location provider warnings
+        "providers":"gps",     // Return GPS, NETWORK and CELL locations
+        "useCache":true,       // Return GPS and NETWORK cached locations
+        "satelliteData":true, // Return of GPS satellite info
+        "buffer":true,        // Buffer location data
+        "bufferSize":3,         // Max elements in buffer
+        "signalStrength":false // Return cell signal strength data
+      });
+  }
+
+  showToast(message){
+    message = message || "null"
+    this.toast.show(message, '5000', 'center').subscribe(
+      toast => {
+        console.log(toast);
+      }
+    );
+    
+  }
 }
