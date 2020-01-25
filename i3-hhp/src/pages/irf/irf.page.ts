40df3abe78c3a0479a3fde9d537c2d7d51b2ffd3
Jonas Arguelles <jonasa@sourcepad.com>
2020-01-25 15:28:37 +0800

    added i3-hhp codes

---
+++ i3-hhp/src/pages/irf/irf.page.ts
@@ -0,0 +1,288 @@
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+import { SMS } from "@ionic-native/sms";
+import { StorageService } from "../../app/services/util/storage.service";
+import { Toast } from "@ionic-native/toast";
+import { NativeStorage } from "@ionic-native/native-storage";
+import { Platform, NavController } from "ionic-angular";
+import { tagpanelPage } from "../tagpanel/tagpanel.page";
+import { HttpClient } from "@angular/common/http";
+import { CsvService } from "../../app/services/util/csv.service";
+declare var AdvancedGeolocation: any;
+
+@Component({
+  selector: "app-irf",
+  templateUrl: "./irf.page.html"
+})
+export class irfPage implements OnInit {
+  formPanel: FormGroup;
+  isBarcodeScanned = false;
+  location;
+  irfObj;
+  showBranchName = false;
+  irdId;
+  requests;
+  list: any;
+  brgy_lookup: any;
+
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private sms: SMS,
+    private storage: StorageService,
+    private fb: FormBuilder,
+    private toast: Toast,
+    private nativeStorage: NativeStorage,
+    private platform: Platform,
+    public navCtrl: NavController,
+    public http: HttpClient,
+    public csvService: CsvService
+  ) {
+    this.initForm();
+  }
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.csvService.loadCSV();
+      this.csvService.brgy_lookup_subject.subscribe(
+        res => {
+          this.brgy_lookup = res;
+          this.get_last_saved();
+          console.log("lookup is ", this.brgy_lookup);
+        },
+        err => {
+          console.log("something went wrong");
+        }
+      );
+    });
+  }
+
+  initForm() {
+    this.formPanel = this.fb.group({
+      fi_name: ["", [Validators.required]],
+      panel_type: ["", [Validators.required]],
+      start_hatching: ["", [Validators.required]],
+      panel_code: ["", [Validators.required]],
+      brgy_code: ["", [Validators.required]],
+      brgy_name: ["", [Validators.required]],
+      region: ["", [Validators.required]],
+      province: ["", [Validators.required]],
+      municipality: ["", [Validators.required]],
+      urbanity: ["", [Validators.required]]
+    });
+    this.formPanel.valueChanges.subscribe(value => {
+      this.save_last_saved();
+    });
+    this.formPanel.get("brgy_code").valueChanges.subscribe(value => {
+      this.findByBrgyCode(value);
+    });
+  }
+
+  findByBrgyCode(value) {
+    let matchValue = "";
+    matchValue = this.brgy_lookup.filter(val => value == val.brgy_code);
+    if (matchValue.length > 0) {
+      value = matchValue[0];
+      this.formPanel.get("brgy_name").setValue(value.brgy);
+      this.formPanel.get("municipality").setValue(value.municipality);
+      this.formPanel.get("province").setValue(value.province);
+      this.formPanel.get("region").setValue(value.region);
+      this.formPanel.get("urbanity").setValue(value.urbanity);
+    }
+  }
+
+  // findByPanelCode(panelcode) {
+  //   this.storage.getAllItem().then(
+  //     data => {
+  //       let objects = <any[]>data;
+  //       console.log("objects", objects);
+  //       let item = objects.filter(
+  //         res =>
+  //           !!res["panel_code"] &&
+  //           res["panel_code"] == panelcode &&
+  //           res["last"] == true
+  //       );
+
+  //       console.log("last item", item);
+  //       if (item.length > 0) {
+  //         //with name
+  //         if (!!item[0]["panel_code"]) {
+  //           let object = {
+  //             region: item[0]["region"],
+  //             panel_name: item[0]["panel_name"]
+  //           };
+  //           this.formPanel.patchValue(object, {
+  //             emitEvent: false,
+  //             onlySelf: true
+  //           });
+  //         }
+  //       }
+  //     },
+  //     error => console.error(error)
+  //   );
+  // }
+
+  save_last_saved() {
+    console.log("saving", this.formPanel.value);
+    this.nativeStorage.setItem("last_saved", this.formPanel.value).then(
+      () => {
+        console.log("Stored last item!");
+      },
+      error => console.error("Error storing item", error)
+    );
+  }
+
+  ionViewDidEnter() {
+    this.getAllItems();
+  }
+
+  get_last_saved() {
+    this.nativeStorage.getItem("last_saved").then(
+      data => {
+        console.log("retrieved last item", data);
+        if (!!data) {
+          this.formPanel.patchValue(data);
+        }
+      },
+      error => console.error("Error storing item", error)
+    );
+  }
+
+  // formatDate(date) {
+  //   // mm/dd/yy format
+  //   let d = date.getDate();
+  //   let m = date.getMonth() + 1; //Month from 0 to 11
+  //   let y = date.getFullYear();
+  //   return "" + (m <= 9 ? "0" + m : m) + "/" + (d <= 9 ? "0" + d : d) + "/" + y;
+  // }
+
+  // save(value) {
+  //   this.storage.setItem("irf", value);
+  //   this.navCtrl.push(tagpanelPage, {
+  //     irfId: this.storage.itemId
+  //   });
+  // }
+
+  search(value) {
+    let panelcode = this.formPanel.value["panel_code"];
+    let id = "hhp" + panelcode;
+    //find by panel_code week_code name
+
+    this.storage.getItem(id).then(
+      data => {
+        console.log("retrieved", data);
+        let foundData: any = data;
+        if (!!foundData.Id) {
+          this.navCtrl.push(tagpanelPage, {
+            irfId: foundData.Id
+          });
+        } else {
+          this.save(this.formPanel.value);
+        }
+      },
+      error => {}
+    );
+  }
+
+  save(value) {
+    this.storage.setItem("hhp", value);
+    this.navCtrl.push(tagpanelPage, {
+      irfId: this.storage.itemId
+    });
+  }
+
+  scan() {
+    this.isBarcodeScanned = false;
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        console.log("Barcode data", barcodeData);
+        this.isBarcodeScanned = true;
+        this.formPanel.get("panel_code").setValue(barcodeData.text);
+        this.showToast("Successfully Scanned");
+      })
+      .catch(err => {
+        this.showToast(err);
+        console.log("Error", err);
+      });
+  }
+
+  clear() {
+    this.formPanel.reset();
+  }
+
+  // findName(panelcode) {
+  //   this.storage.getAllItem().then(
+  //     data => {
+  //       let objects = <any[]>data;
+  //       console.log("objects", objects);
+  //       let item = objects.filter(
+  //         res =>
+  //           !!res["panel_code"] &&
+  //           res["panel_code"] == panelcode &&
+  //           res["last"] == true
+  //       );
+
+  //       console.log("last item", item);
+  //       if (item.length > 0) {
+  //         //with name
+  //         if (!!item[0]["panel_code"]) {
+  //           let object = Object.assign(
+  //             {
+  //               region: item[0]["region"],
+  //               panel_name: item[0]["panel_name"]
+  //             },
+  //             this.formPanel.value
+  //           );
+  //           this.save(object);
+  //         } else {
+  //           this.save(this.formPanel.value);
+  //         }
+  //       } else {
+  //         //wala
+  //         this.save(this.formPanel.value);
+  //       }
+  //     },
+  //     error => console.error(error)
+  //   );
+  // }
+
+  showToast(message) {
+    message = message || "null";
+    this.toast.show(message, "5000", "top").subscribe(toast => {
+      console.log(toast);
+    });
+  }
+
+  getAllItems() {
+    this.storage.getAllItem().then(
+      data => {
+        let objects = <any[]>data;
+        this.list = objects.filter(
+          res => !!res["Id"] && res["Id"].includes("hhp")
+        );
+        console.log("list", this.list);
+      },
+      error => console.error(error)
+    );
+  }
+
+  removeRequest(key) {
+    this.storage.removeItem(key).then(
+      data => {
+        this.getAllItems();
+      },
+      error => console.error(error)
+    );
+  }
+
+  moveToIrf(row: any) {
+    if (!!row) {
+      this.navCtrl.push(tagpanelPage, {
+        irfId: row.Id
+      });
+    } else {
+      this.navCtrl.push(tagpanelPage, {});
+    }
+  }
+}
---
+++ i3-hhp/src/pages/tagpanel/tagpanel.page.ts
@@ -0,0 +1,541 @@
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+import { SMS } from "@ionic-native/sms";
+import { StorageService } from "../../app/services/util/storage.service";
+import { Toast } from "@ionic-native/toast";
+import { NativeStorage } from "@ionic-native/native-storage";
+import { irfPage } from "../irf/irf.page";
+import { NavController, NavParams, Platform } from "ionic-angular";
+import { PictureSourceType, CameraOptions, Camera } from "@ionic-native/camera";
+import { FilePath } from "@ionic-native/file-path";
+import { File } from "@ionic-native/file";
+import { Network } from "@ionic-native/network";
+import { HttpService } from "../../app/services/util/http.service";
+import { Base64 } from "@ionic-native/base64";
+declare var AdvancedGeolocation: any;
+
+@Component({
+  selector: "app-tagpanel",
+  templateUrl: "./tagpanel.page.html"
+})
+export class tagpanelPage implements OnInit {
+  // url = "http://10.0.2.2:3000";
+  url = "http://api.uniserve.ph";
+
+  formPanel: FormGroup;
+  isBarcodeScanned = false;
+  location;
+  tagPanelObj;
+  showZeroRemarks = false;
+  irfId;
+  irfObj: any = {};
+  date_visit;
+
+  isCaptureLoc = false;
+  connectedToNet = false;
+  houseImagePathNative;
+  houseImageImagePath;
+
+  uploading = false;
+  saving = false;
+  takingPicture = false;
+
+  private win: any = window;
+
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private sms: SMS,
+    public camera: Camera,
+    private filePath: FilePath,
+    private storage: StorageService,
+    private file: File,
+    private network: Network,
+    private base64: Base64,
+    private fb: FormBuilder,
+    private toast: Toast,
+    private nativeStorage: NativeStorage,
+    public navCtrl: NavController,
+    private navParams: NavParams,
+    private platform: Platform,
+    private httpService: HttpService
+  ) {
+    this.formPanel = fb.group({
+      Id: ["", [Validators.required]],
+      gps_location: ["", [Validators.required]],
+      loc_accuracy: ["", [Validators.required]],
+      panel_code: ["", [Validators.required]],
+      panel_name: ["", [Validators.required]],
+      sec: ["", [Validators.required]],
+      kids: ["", [Validators.required]],
+      panelist_age: ["", [Validators.required]],
+      work_status: ["", [Validators.required]],
+      hh_size: ["", [Validators.required]],
+      redemption_mode: ["", [Validators.required]],
+      contact_number: [
+        "",
+        [Validators.required, Validators.pattern("^(09|\\+639)\\d{9}$")]
+      ],
+      fi_name: ["", [Validators.required]],
+      panel_type: ["", [Validators.required]],
+      start_hatching: ["", [Validators.required]],
+      brgy_code: ["", [Validators.required]],
+      brgy_name: ["", [Validators.required]],
+      region: ["", [Validators.required]],
+      province: ["", [Validators.required]],
+      municipality: ["", [Validators.required]],
+      urbanity: ["", [Validators.required]]
+    });
+    // add field here
+  }
+
+  ngOnInit() {
+    console.log("tag panel init");
+
+    this.irfId = this.navParams.get("irfId") || "";
+    this.formPanel.valueChanges;
+    console.log("irfid", this.irfId);
+    if (!!this.irfId) {
+      this.nativeStorage.getItem(this.irfId).then(
+        data => {
+          this.irfObj = data;
+          console.log("retrieved data", this.irfObj);
+          this.formPanel.patchValue(data);
+          this.houseImageImagePath = this.getTrustImg(
+            this.irfObj["house_image_path"]
+          );
+          this.houseImagePathNative = this.irfObj["house_image_path_native"];
+        },
+        error => console.error("Error storing item", error)
+      );
+    }
+  }
+
+  ionViewDidEnter() {
+    this.platform.ready().then(() => {
+      if (this.network.type != "none") {
+        console.log("network connected!");
+        this.connectedToNet = true;
+      } else {
+        this.connectedToNet = false;
+      }
+      this.watchNetwork();
+    });
+  }
+
+  scan() {
+    this.isBarcodeScanned = false;
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        console.log("Barcode data", barcodeData);
+        this.isBarcodeScanned = true;
+        this.formPanel.get("panel_code").setValue(barcodeData.text);
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+
+  sendSMS() {
+    console.log("sending", this.formatMessage());
+    if (this.isDataValid(this.formPanel.value)) {
+      this.sms.send("09177131456", this.formatMessage()).then(
+        () => {
+          this.save(this.formPanel.value);
+          console.log("message sent");
+        },
+        error => console.error("Error removing item", error)
+      );
+    }
+  }
+
+  formatMessage() {
+    this.date_visit = new Date().toLocaleString();
+    let v = this.formPanel.value;
+
+    let message =
+      "HHP;" +
+      `${v.fi_name || ""};` +
+      `${v.panel_code || ""};` +
+      `${v.panel_name || ""};` +
+      `${v.sec || ""};` +
+      `${v.kids || ""};` +
+      `${v.panelist_age || ""};` +
+      `${v.work_status || ""};` +
+      `${v.hh_size || ""};` +
+      `${v.redemption_mode || ""};` +
+      `${v.contact_number || ""};` +
+      `${v.brgy_code || ""};` +
+      `${v.brgy_name || ""};` +
+      `${v.region || ""};` +
+      `${v.province || ""};` +
+      `${v.municipality || ""};` +
+      `${v.urbanity || ""};` +
+      `${v.start_hatching || ""};` +
+      `${this.date_visit || ""};` +
+      `${v.start_hatching || ""};` +
+      `${v.panel_type || ""};` +
+      `${v.gps_location || ""};` +
+      `${this.irfObj.gl_name || ""};`;
+    // add field here
+    return message;
+  }
+
+  start() {
+    AdvancedGeolocation.start(
+      success => {
+        try {
+          let jsonObject: any = JSON.parse(success);
+
+          if (!!jsonObject.latitude) {
+            this.location = jsonObject;
+
+            this.formPanel
+              .get("gps_location")
+              .setValue(
+                `${this.location.latitude}, ${this.location.longitude}`
+              );
+            this.formPanel
+              .get("loc_accuracy")
+              .setValue(`${parseFloat(this.location.accuracy.toFixed(2))}`);
+          } else {
+          }
+
+          console.log("Provider now " + JSON.stringify(jsonObject));
+          // this.showToast(JSON.stringify(jsonObject))
+          switch (jsonObject.provider) {
+            case "gps":
+              //TODO
+              break;
+
+            case "network":
+              //TODO
+              break;
+
+            case "satellite":
+              //TODO
+              break;
+
+            case "cell_info":
+              //TODO
+              break;
+
+            case "cell_location":
+              //TODO
+              break;
+
+            case "signal_strength":
+              //TODO
+              break;
+          }
+        } catch (exc) {
+          //this.showToast("value"+exc)
+          console.log("Invalid JSON: " + exc);
+        }
+      },
+      error => {
+        this.showToast(JSON.stringify(error));
+        console.log("ERROR! " + JSON.stringify(error));
+      },
+      ////////////////////////////////////////////
+      //
+      // REQUIRED:
+      // These are required Configuration options!
+      // See API Reference for additional details.
+      //
+      ////////////////////////////////////////////
+      {
+        minTime: 500, // Min time interval between updates (ms)
+        minDistance: 1, // Min distance between updates (meters)
+        noWarn: true, // Native location provider warnings
+        providers: "gps", // Return GPS, NETWORK and CELL locations
+        useCache: true, // Return GPS and NETWORK cached locations
+        satelliteData: true, // Return of GPS satellite info
+        buffer: true, // Buffer location data
+        bufferSize: 3, // Max elements in buffer
+        signalStrength: false // Return cell signal strength data
+      }
+    );
+  }
+
+  save(value) {
+    this.resetOthePanelCodesLast();
+  }
+
+  actualSave() {
+    this.saving = true;
+    let value = this.formPanel.value;
+    this.date_visit = new Date().toLocaleString();
+    if (this.isDataValid(value)) {
+      value["date_visit"] = this.date_visit;
+      value["last"] = true;
+      this.saveOnly(value);
+      this.navCtrl.pop();
+    }
+  }
+
+  saveImage(value) {
+    this.saving = true;
+    this.date_visit = new Date().toLocaleString();
+    value["house_image_path"] = this.houseImageImagePath;
+    value["house_image_path_native"] = this.houseImagePathNative;
+    value["date_visit"] = this.date_visit;
+
+    this.saveOnly(value);
+    this.saving = false;
+  }
+
+  saveOnly(value) {
+    if (!!this.irfObj && !!this.irfObj.Id) {
+      value = Object.assign(this.irfObj, value);
+    }
+    console.log("storing a", value);
+    this.storage.setItem("hhp", value);
+  }
+
+  resetOthePanelCodesLast() {
+    this.storage.getAllItem().then(
+      data => {
+        let objects = <any[]>data;
+        let items = objects.filter(
+          res =>
+            !!res["panel_code"] &&
+            res["panel_code"] == this.formPanel.value.panel_code &&
+            res["last"] == true
+        );
+        items.forEach(value => {
+          console.log("setting to false", value);
+          value["last"] = false;
+          this.storage.setItem("hhp", value);
+        });
+        setTimeout(() => {}, 500);
+        this.actualSave();
+      },
+      error => console.error(error)
+    );
+  }
+
+  isDataValid(value) {
+    return true;
+    // if (value.fw_result == "") {
+    //   this.showToast("FW Result shout not be blank");
+    //   return false;
+    // } else if (value.fw_type == "") {
+    //   this.showToast("FW Type should not be blank");
+    //   return false;
+    // } else {
+    //   return true;
+    // }
+  }
+
+  showToast(message) {
+    message = message || "null";
+    this.toast.show(message, "5000", "top").subscribe(toast => {
+      console.log(toast);
+    });
+  }
+
+  captureLoc(value) {
+    this.isCaptureLoc = value;
+    if (!value) {
+      AdvancedGeolocation.stop();
+    } else {
+      this.start();
+    }
+  }
+
+  takePicture(
+    type,
+    sourceType: PictureSourceType = this.camera.PictureSourceType.CAMERA
+  ) {
+    this.takingPicture = true;
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera
+      .getPicture(options)
+      .then(imageData => {
+        this.filePath
+          .resolveNativePath(imageData)
+          .then(path => {
+            console.log(imageData, path);
+            let imagePath = path.substr(0, path.lastIndexOf("/") + 1);
+            let imageName = path.substring(
+              path.lastIndexOf("/") + 1,
+              path.length
+            );
+
+            if (this.platform.is("android")) {
+              this.file
+                .checkDir(this.file.externalRootDirectory, "hhp")
+                .then(response => {
+                  console.log("Directory exists" + response);
+                  this.moveToFile(imagePath, imageName, type);
+                })
+                .catch(err => {
+                  console.log("Directory doesn't exist" + JSON.stringify(err));
+                  this.file
+                    .createDir(this.file.externalRootDirectory, "hhp", false)
+                    .then(response => {
+                      console.log("Directory create" + response);
+                      this.moveToFile(imagePath, imageName, type);
+                    })
+                    .catch(err => {
+                      console.log("Directory no create" + JSON.stringify(err));
+                    });
+                });
+            }
+          })
+          .catch(err => {
+            debugger;
+            console.error(err);
+          });
+      })
+      .catch(err => {
+        debugger;
+        console.error(err);
+      });
+  }
+
+  moveToFile(imagePath, imageName, type) {
+    let panel_code = this.formPanel.get("panel_code").value;
+    this.file
+      .moveFile(
+        imagePath,
+        imageName,
+        this.file.externalRootDirectory + "hhp/",
+        panel_code + "_" + "HHP_HSE_panelcode" + ".jpeg"
+      )
+      .then(newFile => {
+        switch (type) {
+          case "House_Image":
+            this.houseImageImagePath = this.getTrustImg(newFile.nativeURL);
+            this.houseImagePathNative = newFile.nativeURL;
+            break;
+          default:
+        }
+        if (!!this.irfObj.Id) {
+          this.saveImage(this.formPanel.value);
+          this.takingPicture = false;
+        }
+
+        console.log(newFile);
+      })
+      .catch(err => {
+        debugger;
+        console.error(err);
+      });
+  }
+
+  getTrustImg(imageSrc) {
+    let path = this.win.Ionic.WebView.convertFileSrc(imageSrc);
+    console.log(path);
+    return path;
+  }
+
+  watchNetwork() {
+    console.log("watching network");
+    this.network.onDisconnect().subscribe(() => {
+      this.showToast("Disconnected from network");
+      console.log("network disconnected!");
+      this.connectedToNet = false;
+    });
+
+    this.network.onConnect().subscribe(() => {
+      console.log("network connected!");
+      // We just got a connection but we need to wait briefly
+      // before we determine the connection type. Might need to wait.
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        this.showToast("Connected to network");
+        this.connectedToNet = true;
+      }, 1000);
+    });
+  }
+
+  sync() {
+    console.log("sending", this.formPanel.value);
+    this.uploading = true;
+    this.httpService
+      .post(this.url + "/hh_recruitments", this.formPanel.value, false)
+      .timeout(10000)
+      .subscribe(
+        data => {
+          if (data["success"] == true) {
+            let value = this.formPanel.value;
+            value["stored"] = true;
+            value["id"] = data["id"];
+            if (!!this.irfObj && !!this.irfObj.Id) {
+              value = Object.assign(this.irfObj, value);
+            }
+            this.irfObj["stored"] = true;
+            this.irfObj["id"] = value["id"];
+            this.storage.setItem("hhp", value);
+            this.uploading = false;
+            this.showToast("Data Uploaded");
+            this.sendSMS();
+          }
+        },
+        err => {
+          debugger;
+          this.uploading = false;
+          this.showToast("Something went wrong" + err.message);
+          console.log(err);
+        }
+      );
+  }
+
+  upload(type, filePath) {
+    let fileName = filePath.split("/").pop();
+    let path = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+
+    this.base64.encodeFile(filePath).then(
+      (base64File: string) => {
+        let value = {};
+        switch (type) {
+          case "House_Image":
+            value["house_image"] = base64File;
+            break;
+        }
+        this.uploadPhoto(value, type);
+        console.log("here is encoded image ", base64File);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  uploadPhoto(value, type) {
+    this.uploading = true;
+    this.httpService
+      .patch(
+        this.url + "/hh_recruitments/" + this.irfObj["id"] + "/upload",
+        value,
+        false
+      )
+      .timeout(10000)
+      .subscribe(
+        data => {
+          value[type + "stored"] = true;
+          this.irfObj[type + "stored"] = true;
+          if (!!this.irfObj && !!this.irfObj.Id) {
+            value = Object.assign(this.irfObj, value);
+          }
+          this.storage.setItem("request", value);
+          this.uploading = false;
+          this.showToast("Image Uploaded");
+        },
+        err => {
+          debugger;
+          this.uploading = false;
+          this.showToast("Something went wrong" + err.message);
+          console.log(err);
+        }
+      );
+  }
+}
